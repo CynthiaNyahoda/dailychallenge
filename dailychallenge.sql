b2135aeb90bb9348c9cb786da67e058df044a1fd
CynthiaNyahoda <cynthianyahoda19@gmail.com>
2023-02-22 13:26:23 +0400

    submit

---
+++ dailychallenge.sql
@@ -0,0 +1,23 @@
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Matt','Damon','08/10/1970', 5);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('George','Clooney','06/05/1961', 2);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Cynthia','Nyahoda','19/01/01', 10);
+
+INSERT INTO actors(first_name, last_name, age, number_oscars)
+VALUES('Michael B','Jordan', '09/02/1987', 7);
+
+INSERT INTO actors(first_name, last_name, age, number_oscars)
+VALUES('Viola','Davis', '11/08/1965', 9);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES
+    ('Vin', 'Diesel', '18/07/1967', 4),
+	('Emma', 'Stone', '06/11/1988', 6),
+    ('Sylvester','Stallone','06/07/1946', 12);
+    
+	
+	
